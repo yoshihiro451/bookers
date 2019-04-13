cb226aad904ed3efba629566fec03c48b28bbff1
yoshihiro451 <y09076739471y@gmail.com>
2019-04-13 07:14:08 +0000

    finish

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,37 @@
+class PostsController < ApplicationController
+  def new
+  	@post = Post.new
+  end
+  def create
+  	post = Post.new(post_params)
+  	post.save
+  	redirect_to post_path(post.id)
+  	flash[:hoge] = "successfully"
+  end
+  def show
+       @post = Post.find(params[:id])
+  end
+
+  def index
+  	@postt = Post.new
+  	@posts = Post.all
+  end
+  def edit
+        @post = Post.find(params[:id])
+  end
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+    flash[:hoge] = "successfully"
+  end
+  def destroy
+  	post = Post.find(params[:id])
+  	post.destroy
+  	redirect_to posts_path
+  end
+  private
+    def post_params
+        params.require(:post).permit(:title, :body)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get '/' => 'info#index'
+    root "root#top"
+    
+	post "/posts" => "posts#create"
+	get '/posts' => 'posts#index', as: "index"
+	patch '/posts/:id' => 'posts#update', as: 'update_post'
+	get '/posts/:id/edit' => 'posts#edit', as: 'edit_post'
+	delete '/posts/:id' => 'posts#destroy', as: 'destroy_post'
+	get "/posts/:id" => "posts#show", as: "post"
+
+
+
+
 end
